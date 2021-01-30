47446ee41a794f5c8b665f383e52f7ed62ee830a
lunjoa <joakim97lundstrom@gmail.com>
2021-01-30 18:34:42 +0000

    Implemented controller support and http get

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-const piblaster = require("piblaster.js");
-
-app.get("/", (request, response) => {
-	response.send("Hello World!");
-});
+const piblaster = require("pi-blaster.js");
 
 app.listen(port, () => {
-	console.log("Example app listening at http://localhost:", port);
+	console.log("Started on port ", port);
 });
 
-// var HID = require("node-hid");
-// console.log(HID.devices());
-// var hid = new HID.HID(11720, 24834);
-// hid.on("data", function (data) {
-// 	console.log(data[1]);
+app.get("/steering/:value", (request, response) => {
+	steer(request.params.value);
+	response.send(request.params.value);
+});
+
+app.get("/throttle/:value", (request, response) => {
+	throttle(request.params.value);
+	response.send(request.params.value);
+});
+
+
+function steer(value){
+	const filteredValue = Math.min(Math.max(value, -100), 100)
+	const convertedValue = (195 + filteredValue/3)/1000;
+	console.log("Setting steering to: ",convertedValue);
+        piblaster.setPwm(17, convertedValue);
+}
+
+function throttle(value){
+	const filteredValue = Math.min(Math.max(value, -100), 100)
+	const convertedValue = (150 - filteredValue/5)/1000;
+	console.log("Setting throttle to: ",convertedValue);
+        piblaster.setPwm(18, convertedValue);
+}
+
+//var HID = require("node-hid");
+//console.log(HID.devices());
+//var hid = new HID.HID(11720, 24834);
+//hid.on("data", function (data) {
+//	console.log(data[1]);
 // });
-var HID = require("node-hid");
+try{var HID = require("node-hid");
 console.log(HID.devices());
 const Gamecontroller = require("gamecontroller");
 const ctrl = new Gamecontroller("bitdo_Dinput");
@@ -26,9 +47,14 @@ ctrl.connect(function () {
 	console.log("connected");
 });
 ctrl.on("JOYL:move", function (position) {
-	console.log("Left stick was moved: ", position);
-	piblaster.setPwm(17, 0.1 + position.x / 1500);
+	const value = position.x / 1.275 - 100;
+	steer(value);
 });
+ctrl.on("JOYR:move", function (position) {
+	const value = position.y / 1.275 - 100;
+	throttle(value)
+});}
+catch{console.log("could not use controller");}
 // ctrl.on("X:press", function () {
 // 	console.log("X was pressed");
 // });
